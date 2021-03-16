9f570b3e626d9a33870a6e4a464dc83d7dcbf55a
Jeffrey Wong <jwong@dayspring-tech.com>
2021-03-16 16:17:37 -0700

    fix up webhook handling

--- src/Controller/WebhookController.php
+++ src/Controller/WebhookController.php
@@ -4,13 +4,15 @@ namespace CodeCloud\Bundle\ShopifyBundle\Controller;
 
 use CodeCloud\Bundle\ShopifyBundle\Event\WebhookEvent;
 use CodeCloud\Bundle\ShopifyBundle\Model\ShopifyStoreManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
-class WebhookController
+class WebhookController extends Controller
 {
     /**
      * @var ShopifyStoreManagerInterface
@@ -38,8 +40,15 @@ class WebhookController
      */
     public function handleWebhook(Request $request)
     {
-        $topic     = $request->query->get('topic');
-        $storeName = $request->query->get('store');
+        $oauthSharedSecret = $this->getParameter('codecloud_shopify.oauth.shared_secret');
+        $hmac_raw = hash_hmac('sha256', $request->getContent(), $oauthSharedSecret, true);
+        $hmac_base64 = base64_encode($hmac_raw);
+        if ($hmac_base64 !== $request->headers->get('x-shopify-hmac-sha256')) {
+            throw new HttpException(403, 'hmac does not match');
+        }
+
+        $topic     = $request->headers->get('x-shopify-topic');
+        $storeName = $request->headers->get('x-shopify-shop-domain');
 
         if (!$topic || !$storeName) {
             throw new NotFoundHttpException();
